8d3c424c6e1d7631eb9c26606ad6e841e119f570
your <leonardoxrrp@gmail.com>
2020-11-21 17:48:47 +0100

    setting up map

---
+++ src/Map.js
@@ -0,0 +1,28 @@
+import GoogleMapReact from "google-map-react"
+import LocationMarker from "./LocationMarker"
+const { REACT_APP_APIMAP } = process.env;
+
+function Map({center, zoom}) {
+    return (
+        <div className="map">
+            <GoogleMapReact
+                    bootstrapURLKeys={{ key: REACT_APP_APIMAP}}
+                    defaultCenter={center}
+                    defaultZoom={zoom}
+            >
+                <LocationMarker lat = {center.lat}  lng={center.lng}/>
+            </GoogleMapReact>
+           { console.log(process.env.REACT_APP_APIMAP)}
+        </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 42.3265,
+        lng: -122.8756
+    },
+    zoom: 6
+    }
+
+export default Map
